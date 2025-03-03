e669d2f4f4311331f047054374c5ddb22c56c8ad
praveengak <arugilpraveen@gmail.com>
2025-03-03 08:25:14 +0530

    Implementing ResponseEntity

--- src/main/java/com/todo/todoProject/controller/TodoController.java
+++ src/main/java/com/todo/todoProject/controller/TodoController.java
@@ -2,7 +2,9 @@ package com.todo.todoProject.controller;
 
 import com.todo.todoProject.model.Todo;
 import com.todo.todoProject.service.TodoService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,30 +14,37 @@ import java.util.List;
 @RequestMapping("/api")
 public class TodoController {
 
-    @Autowired
+
     TodoService todoService;
 
+    public TodoController(TodoService todoService){
+        this.todoService = todoService;
+    }
+
     @PostMapping("/add")
-    public void addTask(@RequestBody Todo todo) {
+    public ResponseEntity<String> addTask(@RequestBody Todo todo) {
         todoService.addTask(todo);
+        return ResponseEntity.ok("Task has been added");
     }
 
     /*
     in the blow method I should be able to update the existing todolist
      */
     @PutMapping("/update")
-    public void updateTask(@RequestBody Todo todo){
+    public ResponseEntity<String> updateTask(@RequestBody Todo todo){
         todoService.updateTask(todo);
+        return ResponseEntity.ok("Task has been updated");
     }
 
     @GetMapping("/listOfAllTodoTask")
-    public List<Todo> listOfAllTodoTask(){
-        return todoService.listOfAllTodoTask();
+    public ResponseEntity<List<Todo>> listOfAllTodoTask(){
+        return ResponseEntity.ok(todoService.listOfAllTodoTask());
     }
 
     @DeleteMapping("/delete/{taskId}")
-    public void deleteTodo(@PathVariable int taskId){
+    public ResponseEntity<String> deleteTodo(@PathVariable int taskId){
         todoService.deleteTodo(taskId);
+        return ResponseEntity.ok("Task has been deleted");
     }
 
 }
